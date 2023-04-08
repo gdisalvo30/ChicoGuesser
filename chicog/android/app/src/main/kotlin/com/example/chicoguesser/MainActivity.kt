088a1c642d1dde5f389aae535737fa2d37cfb140
lkoker <123502003+lkoker@users.noreply.github.com>
2023-04-08 09:27:29 -0700

    init commit

---
+++ chicog/android/app/src/main/kotlin/com/example/chicoguesser/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.chicoguesser
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ chicog/lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int num=0;
+  void add(){
+    num++;
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Welcome to CHICOGUESSER',
+            ),
+            FloatingActionButton(
+              onPressed: add,
+              tooltip: "Play button",
+              child: const Icon(Icons.play_circle),  
+            ),
+            FloatingActionButton(
+              onPressed: add,
+              tooltip: "Add image",
+              child: const Icon(Icons.camera_alt_rounded),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
