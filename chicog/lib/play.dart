30fb56c871cf37e2fb86912d0905717a75b89659
lkoker <123502003+lkoker@users.noreply.github.com>
2023-04-27 11:50:34 -0700

    image and text box

--- chicog/lib/play.dart
+++ chicog/lib/play.dart
@@ -10,68 +10,112 @@ class PlayScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        actions: <Widget>[
-          Padding(
-            padding: const EdgeInsets.only(right: 10.0),
-            child: GestureDetector(
-                onTap: () {
-                  Navigator.push<void>(
-                    context,
-                    MaterialPageRoute<void>(
-                        builder: (BuildContext context) =>
-                            const ProfileScreen()),
-                  );
-                },
-                child: const Icon(
-                  Icons.person_sharp,
-                  size: 26.0,
-                )),
-          )
-        ],
-      ),
-    );
-  }
-
-  List<Widget> curImage() {
-    return <Widget>[
-      StreamBuilder(
-        stream: FirebaseFirestore.instance.collection('photos').snapshots(),
-        builder: (context, snapshot) {
-          switch (snapshot.connectionState) {
-            case ConnectionState.waiting:
-              return const CircularProgressIndicator();
-            default:
-              if (snapshot.hasError) {
-                return Text('Error: ${snapshot.error}');
-              } else {
-                return Expanded(
-                  child: ListView.builder(
-                    itemCount: 1,
-                    itemBuilder: (context, index) {
-                      Random random;
-                      random = Random();
-                      index = random.nextInt(snapshot.data!.docs.length);
-                      return photoWidget(snapshot, index);
+        appBar: AppBar(
+          actions: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(right: 10.0),
+              child: GestureDetector(
+                  onTap: () {
+                    Navigator.push<void>(
+                      context,
+                      MaterialPageRoute<void>(
+                          builder: (BuildContext context) =>
+                              const ProfileScreen()),
+                    );
+                  },
+                  child: const Icon(
+                    Icons.person_sharp,
+                    size: 26.0,
+                  )),
+            )
+          ],
+        ),
+        body: Center(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Expanded(
+                  flex: 1,
+                  child: StreamBuilder(
+                    stream: FirebaseFirestore.instance
+                        .collection('photos')
+                        .snapshots(),
+                    builder: (context, snapshot) {
+                      switch (snapshot.connectionState) {
+                        case ConnectionState.waiting:
+                          return const CircularProgressIndicator();
+                        default:
+                          if (snapshot.hasError) {
+                            return Text('Error: ${snapshot.error}');
+                          } else {
+                            return Expanded(
+                              child: ListView.builder(
+                                itemCount: 1,
+                                itemBuilder: (context, index) {
+                                  Random random;
+                                  random = Random();
+                                  index = random
+                                      .nextInt(snapshot.data!.docs.length);
+                                  return photoWidget(snapshot, index);
+                                },
+                              ),
+                            );
+                          }
+                      }
                     },
                   ),
-                );
-              }
-          }
-        },
-      )
-    ];
+                ),
+                const Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Guess',
+                    ),
+                  )
+                ),
+              ]),
+        ));
   }
+}
 
-  Widget photoWidget(AsyncSnapshot<QuerySnapshot> snapshot, int index) {
-    try {
-      return Column(
-        children: [
-          Image.network(snapshot.data!.docs[index]['downloadURL']),
-        ],
-      );
-    } catch (e) {
-      return Text('Error: $e');
-    }
+List<Widget> curImage() {
+  return <Widget>[
+    StreamBuilder(
+      stream: FirebaseFirestore.instance.collection('photos').snapshots(),
+      builder: (context, snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.waiting:
+            return const CircularProgressIndicator();
+          default:
+            if (snapshot.hasError) {
+              return Text('Error: ${snapshot.error}');
+            } else {
+              return Expanded(
+                child: ListView.builder(
+                  itemCount: 1,
+                  itemBuilder: (context, index) {
+                    Random random;
+                    random = Random();
+                    index = random.nextInt(snapshot.data!.docs.length);
+                    return photoWidget(snapshot, index);
+                  },
+                ),
+              );
+            }
+        }
+      },
+    )
+  ];
+}
+
+Widget photoWidget(AsyncSnapshot<QuerySnapshot> snapshot, int index) {
+  try {
+    return Column(
+      children: [
+        Image.network(snapshot.data!.docs[index]['downloadURL']),
+      ],
+    );
+  } catch (e) {
+    return Text('Error: $e');
   }
 }
